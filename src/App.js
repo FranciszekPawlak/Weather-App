6dd423767d0d77e9fa38cc43483ce5cc3434142e
Franciszek Pawlak <franciszekpawlakit@gmail.com>
2019-03-27 16:52:09 +0100

    add input and get main data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
+//import Input from "components/Input";
+// import Highchart from "components/Highchart";
+// import FutureWeather from "components/FutureWeather";
+// import Pollution from "components/Pollution";
+const APIkey = "1697193f8750f5ec8d1046c2118876cf";
 class App extends Component {
+  state = {
+    dataWeather: [],
+    weatherIsLoad: false,
+    value: "",
+    hours: []
+  };
+
+  handleCahngeInput = e => this.setState({ value: e.target.value });
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.value.length === 0) return;
+    if (prevState.value !== this.state.value) {
+      const API = `https://api.openweathermap.org/data/2.5/forecast?q=${
+        this.state.value
+      }&appid=${APIkey}&units=metric`;
+      fetch(API)
+        .then(resp => {
+          if (resp.ok) {
+            return resp.json();
+          }
+          throw Error("Brak miasta w bazie");
+        })
+        .then(data =>
+          this.setState({
+            dataWeather: data,
+            weatherIsLoad: true
+          })
+        )
+        .catch(err => console.log(err));
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <div className="App">
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleCahngeInput}
+          />
+          {console.log(this.state.dataWeather)}
+          {/* <Highchart />
+        <FutureWeather />
+        <Pollution /> */}
+        </div>
+      </>
     );
   }
 }
