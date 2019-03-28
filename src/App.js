778986a891337e28e8afd0bd6f7179157638b604
Franciszek Pawlak <franciszekpawlakit@gmail.com>
2019-03-28 21:04:07 +0100

    add weather component and icon files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
 import React, { Component } from "react";
 import "./App.css";
 import Highchart from "./components/Highchart";
-// import FutureWeather from "./components/FutureWeather";
+import Weather from "./components/Weather";
 // import Pollution from "./components/Pollution";
 const APIkey = "1697193f8750f5ec8d1046c2118876cf";
 class App extends Component {
   state = {
+    dataHighchart: [],
     dataWeather: [],
-    weatherIsLoad: false,
+    dataHighchartIsLoad: false,
+    dataWeatherIsLoad: false,
     value: "Warsaw"
   };
 
   handleCahngeInput = e => this.setState({ value: e.target.value });
   componentDidMount() {
-    const API = `https://api.openweathermap.org/data/2.5/forecast?q=${
+    const highchartAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${
       this.state.value
     }&appid=${APIkey}&units=metric`;
-    fetch(API)
+    const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${
+      this.state.value
+    }&appid=${APIkey}&units=metric`;
+
+    fetch(weatherAPI)
       .then(resp => {
         if (resp.ok) {
           return resp.json();
+        } else {
+          this.setState({ dataWeather: [], dataWeatherIsLoad: false });
         }
         throw Error("Brak miasta w bazie");
       })
       .then(data =>
         this.setState({
           dataWeather: data,
-          weatherIsLoad: true
+          dataWeatherIsLoad: true
+        })
+      )
+      .catch(err => console.log(err));
+    fetch(highchartAPI)
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          this.setState({ dataHighchart: [], dataHighchartIsLoad: false });
+        }
+        throw Error("Brak miasta w bazie");
+      })
+      .then(data =>
+        this.setState({
+          dataHighchart: data,
+          dataHighchartIsLoad: true
         })
       )
       .catch(err => console.log(err));
@@ -34,24 +58,45 @@ class App extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (this.state.value.length === 0) return;
     if (prevState.value !== this.state.value && this.state.value.length > 2) {
-      const API = `https://api.openweathermap.org/data/2.5/forecast?q=${
+      const highchartAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${
         this.state.value
       }&appid=${APIkey}&units=metric`;
-      fetch(API)
+      const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${
+        this.state.value
+      }&appid=${APIkey}&units=metric`;
+
+      fetch(weatherAPI)
         .then(resp => {
           if (resp.ok) {
             return resp.json();
-          }
+          } /*else {
+            this.setState({ dataWeather: [], dataWeatherIsLoad: false });
+          }*/
           throw Error("Brak miasta w bazie");
         })
         .then(data =>
           this.setState({
             dataWeather: data,
-            weatherIsLoad: true
+            dataWeatherIsLoad: true
           })
         )
         .catch(err => console.log(err));
-      console.log(this.state.dataWeather);
+      fetch(highchartAPI)
+        .then(resp => {
+          if (resp.ok) {
+            return resp.json();
+          } /*else {
+            this.setState({ dataHighchart: [], dataHighchartIsLoad: false });
+          }*/
+          throw Error("Brak miasta w bazie");
+        })
+        .then(data =>
+          this.setState({
+            dataHighchart: data,
+            dataHighchartIsLoad: true
+          })
+        )
+        .catch(err => console.log(err));
     }
   }
 
@@ -64,8 +109,15 @@ class App extends Component {
             value={this.state.value}
             onChange={this.handleCahngeInput}
           />
-          {this.state.weatherIsLoad ? (
-            <Highchart data={this.state.dataWeather} city={this.state.value} />
+          <br />
+          {this.state.dataWeatherIsLoad ? (
+            <Weather data={this.state.dataWeather} city={this.state.value} />
+          ) : null}
+          {this.state.dataHighchartIsLoad ? (
+            <Highchart
+              data={this.state.dataHighchart}
+              city={this.state.value}
+            />
           ) : null}
         </div>
       </>
